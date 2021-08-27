d904f4a269e25f4344eda57be69208674cba43f0
Beto Perez <jorge.perez@ecart.com>
2021-08-26 21:01:40 -0500

    Add template to group divs

--- src/modules/daybook/views/EntryView.vue
+++ src/modules/daybook/views/EntryView.vue
@@ -1,29 +1,36 @@
 <template>
-  <div class="entry-title d-flex justify-content-between p-2">
+  <template v-if="entry">
+    <div class="entry-title d-flex justify-content-between p-2">
       <div>
-          <span class="text-success fs-3 fw-bold">{{ day }}</span>
-          <span class="mx-1 fs-3">{{month}}</span>
-          <span class="mx-2 fs-4 fw-light">{{yearDay}}</span>
+        <span class="text-success fs-3 fw-bold">{{ day }}</span>
+        <span class="mx-1 fs-3">{{ month }}</span>
+        <span class="mx-2 fs-4 fw-light">{{ yearDay }}</span>
       </div>
 
       <div>
-          <button class="btn btn-danger mx-2">
-              Borrar
-              <i class="fa fa-trash-alt"></i>
-          </button>
-          <button class="btn btn-primary">
-              Subir foto
-              <i class="fa fa-upload"></i>
-          </button>
+        <button class="btn btn-danger mx-2">
+          Borrar
+          <i class="fa fa-trash-alt"></i>
+        </button>
+        <button class="btn btn-primary">
+          Subir foto
+          <i class="fa fa-upload"></i>
+        </button>
       </div>
-  </div>
-  <hr />
-  <div class="d-flex flex-column px-3 h-75">
+    </div>
+    <hr />
+    <div class="d-flex flex-column px-3 h-75">
       <textarea v-model="entry.text" placeholder="Que sucedio hoy?"></textarea>
-  </div>
-  <Fab :icon="'fa-save'"/>
+    </div>
+  <img
+    src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Foswallpapers.com%2Fwp-content%2Fuploads%2F2019%2F04%2Fkate-hazen-pop-m3lvin.jpg&f=1&nofb=1"
+    alt="entry-picture"
+    class="img-thumbnail"
+  />
+  </template>
 
-  <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Foswallpapers.com%2Fwp-content%2Fuploads%2F2019%2F04%2Fkate-hazen-pop-m3lvin.jpg&f=1&nofb=1" alt="entry-picture" class="img-thumbnail">
+  <Fab :icon="'fa-save'" />
+
 </template>
 
 <script>
@@ -32,74 +39,74 @@ import { mapGetters } from 'vuex';
 import getDayMonthYear from '../helpers/getDayMonthYear';
 
 export default {
-    props: {
-        id: {
-            type: String,
-            required: true,
-        },
+  props: {
+    id: {
+      type: String,
+      required: true,
     },
-    components: {
-        Fab: defineAsyncComponent(() => import('../components/Fab.vue'))
-    },
+  },
+  components: {
+    Fab: defineAsyncComponent(() => import('../components/Fab.vue')),
+  },
 
-    data() {
-        return {
-            entry: null,
-        }
-    },
+  data() {
+    return {
+      entry: null,
+    };
+  },
 
-    watch: {
-        id() {
-            this.loadEntry();
-        }
+  watch: {
+    id() {
+      this.loadEntry();
     },
+  },
 
-    created() {
-        this.loadEntry()
+  created() {
+    this.loadEntry();
+  },
+
+  computed: {
+    ...mapGetters('journal', ['getEntryById']),
+    day() {
+      const { day } = getDayMonthYear(this.entry.date);
+      return day;
     },
-
-    computed: {
-        ...mapGetters('journal', ['getEntryById']),
-        day() {
-            const { day } = getDayMonthYear(this.entry.date)
-            return day;
-        },
-        month() {
-            const { month } = getDayMonthYear(this.entry.date)
-            return month;
-        },
-        yearDay() {
-            const { yearDay } = getDayMonthYear(this.entry.date)
-            return yearDay;
-        },
+    month() {
+      const { month } = getDayMonthYear(this.entry.date);
+      return month;
     },
+    yearDay() {
+      const { yearDay } = getDayMonthYear(this.entry.date);
+      return yearDay;
+    },
+  },
 
-    methods: {
-        loadEntry() {
-           const entry = this.getEntryById(this.id);
-           if (!entry) this.$router.push({name: 'no-entry'})
-           this.entry = entry;
-        }
-    }
-}
+  methods: {
+    loadEntry() {
+      const entry = this.getEntryById(this.id);
+      if (!entry) return this.$router.push({ name: 'no-entry' });
+      this.entry = entry;
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 textarea {
-    font-size: 20px;
-    border: none;
-    height: 100%;
+  font-size: 20px;
+  border: none;
+  height: 100%;
 
-    &:focus {
-        outline: none;
-    }
+  &:focus {
+    outline: none;
+  }
 }
 
 img {
-    width: 200px;
-    position: fixed;
-    bottom: 150px;
-    right: 20px;
-    box-shadow: 0 5px 10px rgba($color: #000000, $alpha: 0.2);
+  width: 200px;
+  position: fixed;
+  bottom: 150px;
+  right: 20px;
+  box-shadow: 0 5px 10px rgba($color: #000000, $alpha: 0.2);
 }
 </style>
